d197e9a45cc80c65e702f79b28c009dd75b799a2
allanchuava <allanchuava@gmail.com>
2014-09-03 14:12:53 -0400

    In class 1

---
+++ RStudio Files/AllanChua_Inclass1.R
@@ -0,0 +1,19 @@
+#Allan Chua
+#In class assignment 1
+
+dim(cars1) #lists the dimensions of the data set 50 by 2
+
+var1 <- cars1[2,2] #assign var1 to the content of [2,2]
+var1
+
+colnames(cars1) # the variable names in the set are speed and distance
+
+cars1[,1] # lists the first column
+cars1[,2] # lists the second column
+
+SPEED <- cars[,1]
+SPEED
+
+cars1[15,] #outputs the value of row 15
+
+
